7469bab52ebc9a695c7ff477108e1d05d0053337
Neil <neil@mac.git>
2017-10-15 12:13:43 +0100

    Add TEST3

---
+++ TestCode/Test3/main.cpp
@@ -0,0 +1,42 @@
+// Determine the nth Fibonacci number
+
+
+#include <iostream>
+#include <vector>
+
+#include "Fiblib.hpp"
+
+ // Output vector of ints to the console
+void OutputData(std::vector<int>& vectorData) {
+    for (auto dataItem : vectorData)
+    {
+        std::cout << dataItem << " ";
+    }
+
+    // Blank line
+    std::cout << "\n";
+}
+
+// Main program
+int main(){
+
+    int fibSeqNumber = {};
+    std::cout << "Enter the sequence number : ";
+    std::cin >> fibSeqNumber;
+
+    std::vector<int> fibNumbers {};
+
+    // Try with all parameters
+    fibNumbers = fib(fibSeqNumber, 0, 1);
+    OutputData(fibNumbers);
+
+    // Try less one
+    fibNumbers = fib(fibSeqNumber, 0);
+    OutputData(fibNumbers);
+
+    // Try only one
+    fibNumbers = fib(fibSeqNumber);
+    OutputData(fibNumbers);
+
+}
+

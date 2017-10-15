bfd2aa364bf6690c2c7300bde0849f29fe299e07
Neil <neil@mac.git>
2017-10-15 01:02:12 +0100

    Added Test2

---
+++ TestCode/Test2/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+// Remove the &
+std::string get_hello()
+{
+    std::string str = "Hello";
+    return (str);
+}
+
+// This is probably not good
+void get_hello(std::string& str)
+{
+    str = "Hello";
+}
+
+int main() {
+
+    std::cout << get_hello() << std::endl;
+
+    std::string test {};
+    get_hello(test);
+
+    std::cout << test << std::endl;
+
+    return (EXIT_SUCCESS);
+}
